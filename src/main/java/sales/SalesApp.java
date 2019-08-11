aea4388f8d8053148a81ba65cce6463fb8cca66a
TANGLO2 <1614468806@qq.com>
2019-08-11 13:32:58 +0800

    refator: refactor the SalesApp

--- src/main/java/sales/SalesApp.java
+++ src/main/java/sales/SalesApp.java
@@ -7,57 +7,81 @@ import java.util.List;
 
 public class SalesApp {
 
-	public void generateSalesActivityReport(String salesId, int maxRow, boolean isNatTrade, boolean isSupervisor) {
-		
-		SalesDao salesDao = new SalesDao();
-		SalesReportDao salesReportDao = new SalesReportDao();
-		List<String> headers = null;
-		
-		List<SalesReportData> filteredReportDataList = new ArrayList<SalesReportData>();
-		
-		if (salesId == null) {
-			return;
-		}
-		
-		Sales sales = salesDao.getSalesBySalesId(salesId);
-		
-		Date today = new Date();
-		if (today.after(sales.getEffectiveTo())
-				|| today.before(sales.getEffectiveFrom())){
-			return;
-		}
-		
-		List<SalesReportData> reportDataList = salesReportDao.getReportData(sales);
-		
+	SalesDao salesDao;
+	SalesReportDao salesReportDao;
+	EcmService ecmService;
+
+	public SalesApp() {
+	}
+
+	public SalesApp(SalesDao salesDao, SalesReportDao salesReportDao, EcmService ecmService) {
+		this.salesDao = salesDao;
+		this.salesReportDao = salesReportDao;
+		this.ecmService = ecmService;
+	}
+
+	public void generateSalesActivityReport(String salesId,boolean isNatTrade, boolean isSupervisor, int maxRow){
+		Sales sales = getSales(salesId);
+		List<SalesReportData> salesReportDataList = getsalesReportDataList(sales);
+
+		List<SalesReportData> filteredReportDataList = filterReportDataList(salesReportDataList, isSupervisor, maxRow);
+
+		List<String> headers = getHeaders(isNatTrade);
+
+		SalesActivityReport report = generateReport(headers, salesReportDataList);
+
+		uploadReportDocument(report);
+	}
+
+	private List<SalesReportData> filterReportDataList(List<SalesReportData> reportDataList,boolean isSupervisor, int maxRow) {
+		List<SalesReportData> filteredReportDataList_first = null;
 		for (SalesReportData data : reportDataList) {
 			if ("SalesActivity".equalsIgnoreCase(data.getType())) {
-				if (data.isConfidential()) {
-					if (isSupervisor) {
-						filteredReportDataList.add(data);
-					}
-				}else {
-					filteredReportDataList.add(data);
+				if(data.isConfidential() && !isSupervisor) {
+					continue;
 				}
+				filteredReportDataList_first.add(data);
 			}
 		}
-		
-		List<SalesReportData> tempList = new ArrayList<SalesReportData>();
-		for (int i=0; i < reportDataList.size() || i < maxRow; i++) {
-			tempList.add(reportDataList.get(i));
+		List<SalesReportData> filteredReportDataList_second = new ArrayList<>();
+		for (int i=0; i < filteredReportDataList_first.size() || i < maxRow; i++) {
+			filteredReportDataList_second.add(filteredReportDataList_first.get(i));
 		}
-		filteredReportDataList = tempList;
-		
-		if (isNatTrade) {
-			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Time");
-		} else {
-			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Local Time");
-		}
-		
-		SalesActivityReport report = this.generateReport(headers, reportDataList);
-		
+		return filteredReportDataList_second;
+	}
+
+	private void uploadReportDocument(SalesActivityReport report) {
 		EcmService ecmService = new EcmService();
 		ecmService.uploadDocument(report.toXml());
-		
+	}
+
+
+	private List<String> getHeaders(boolean isNatTrade) {
+		return isNatTrade ? Arrays.asList("Sales ID", "Sales Name", "Activity", "Time")
+				: Arrays.asList("Sales ID", "Sales Name", "Activity", "Local Time");
+	}
+
+	public Sales getSales(String salesId) {
+		if (salesId == null) {
+			return null;
+		}
+		Sales sales = salesDao.getSalesBySalesId(salesId);
+		if(!isSalesIdEffective(sales)){
+			return null;
+		}
+		return sales;
+	}
+
+	public List<SalesReportData> getsalesReportDataList(Sales sales){
+		return salesReportDao.getReportData(sales);
+	}
+
+	public boolean isSalesIdEffective(Sales sales) {
+		Date today = new Date();
+		if (today.after(sales.getEffectiveTo()) || today.before(sales.getEffectiveFrom())){
+			return false;
+		}
+		return true;
 	}
 
 	private SalesActivityReport generateReport(List<String> headers, List<SalesReportData> reportDataList) {
